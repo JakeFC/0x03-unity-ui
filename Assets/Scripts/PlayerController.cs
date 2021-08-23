ffbc275f9dd2d9a1395e6bb430bbc5c9fe77ef21
JakeFC <jacobchavera@yahoo.com>
2021-08-23 12:48:22 -0700

    Task 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	// Our object's Rigidbody component is assigned to rb
+	// in the inspector.
 	public Rigidbody rb;
 	public float speed = 900f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
