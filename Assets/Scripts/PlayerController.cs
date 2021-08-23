7cf696830fbe1db8d6719e0ffff0544bec7dee2e
JakeFC <jacobchavera@yahoo.com>
2021-08-23 12:31:46 -0700

    Task 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float speed = 900f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+		{
+			rb.AddForce(0, 0, speed * Time.deltaTime);
+		}
+		if (Input.GetKey("s"))
+		{
+			rb.AddForce(0, 0, -speed * Time.deltaTime);
+		}
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(-speed * Time.deltaTime, 0, 0);
+		}
+    }
+}
