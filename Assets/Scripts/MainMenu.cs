110c25064b717d94c59151b27eba0b73bfc991b6
JakeFC <jacobchavera@yahoo.com>
2021-08-30 15:11:06 -0700

    Task 10

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,22 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 	public Button playButton;
+	public Button quitButton;
 
 	void Start ()
 	{
 		playButton.GetComponent<Button>().onClick.AddListener(PlayMaze);
+		quitButton.GetComponent<Button>().onClick.AddListener(QuitMaze);
 	}
+
     public void PlayMaze()
 	{
 		SceneManager.LoadScene("Maze");
+	}
+
+	public void QuitMaze()
+	{
+		Debug.Log("Quit Game");
+		Application.Quit();
 	}
 }
